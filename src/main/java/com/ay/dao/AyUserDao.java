041d2eac622151a7efb36d5faf77d53ffc174561
yasa <1394674558@qq.com>
2021-03-04 01:45:32 +0800

    mybatis单表的crud

--- src/main/java/com/ay/dao/AyUserDao.java
+++ src/main/java/com/ay/dao/AyUserDao.java
@@ -10,4 +10,16 @@ public interface AyUserDao {
     List<AyUser> findAll();
 
     AyUser findById(String id);
+
+    List<AyUser> findByName(String name);
+
+    int countByName(String name);
+
+    int insert(AyUser ayUser);
+
+    int update(AyUser ayUser);
+
+    int delete(String id);
+
+    int deleteByName(String name);
 }
